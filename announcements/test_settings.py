4ba54eb475d3779bb3634b66f3cac2da3f9810ab
Jason Ward <jason.ward@policystat.com>
2015-12-09 11:51:27 -0500

    refs #4: Add tests for django 1.8 and use django_nose

--- announcements/test_settings.py
+++ announcements/test_settings.py
@@ -10,4 +10,11 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'announcements',
+    'django_nose',
+]
+
+SECRET_KEY = 'django-announcements-secret-key'
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    '--with-doctest'
 ]
